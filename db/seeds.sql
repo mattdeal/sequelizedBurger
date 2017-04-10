f0d1903f4832d7c769f5c500d415214a37e4d960
mwdeal1 <mwdeal1@gmail.com>
2017-04-10 18:31:14 -0400

    Begin conversion to sequelize

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-use burgers_db;
-INSERT INTO burgers (burger_name, devoured) VALUES ('Small', '0');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Medium', '0');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Large', '0');
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,5 @@
 // Import the ORM to create functions that will interact with the database.
+//todo: remove all ORM references
 var orm = require("../config/orm.js");
 
 var burger = {
